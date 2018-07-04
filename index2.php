c9c4371bff8f3eb63217ed4daeefc044f95e0cf5
wagnerj26 <gwagnerj@gmail.com>
2018-07-04 15:00:31 -0400

    just changing from Crud to qrp

--- index2.php
+++ index2.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.QRProblems.org/crud/index.php"); /* Redirect browser to game so we can just use the shorter url */
+header("Location: https://www.QRProblems.org/qrp/index.php"); /* Redirect browser to game so we can just use the shorter url */
 exit();
 ?>
--- index4.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.QRProblems.org/crud/QRPRepo.php"); /* Redirect browser to game so we can just use the shorter url */
-exit();
-?>
