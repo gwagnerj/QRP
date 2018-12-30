d6eeb5df5ef2a283e6c51491e3d53dc6da6cc95b
gwagnerj <38090331+gwagnerj@users.noreply.github.com>
2018-12-30 05:35:23 -0500

    added the reference to JQuery and other external files as a single reference
    THis should make it easier to change all the files with a single change in refHeader

---
+++ refHeaders.js
@@ -0,0 +1,3 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> 
+src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js">
+src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js">
