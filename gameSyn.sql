3aba63d4571ba479c99d3be7d9d8746a02401fe3
gwagnerj <38090331+gwagnerj@users.noreply.github.com>
2019-01-19 05:48:35 -0500

    added the units checker to the excel solver template and started thnking about sinking phones for the game

--- gameSyn.sql
+++ gameSyn.sql
@@ -1,9 +1,16 @@
 /* this is just to set up a synchonzied timer and eventually the game to coordinate all of the phones.
-The first step is just to make a simple count down timer that can be used across phones/*
+The first step is just to make a simple count down timer that can be used across phones */
 
-CREATE TABLE IF NOT EXISTS `game` (
+CREATE TABLE IF NOT EXISTS `Game` (
   `game_id` int(15) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `roomNum` Int(7) NOT NULL ,
   `groupNum` int(5) NOT NULL,
    `studentNum` int(5) NOT NULL 
 ) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/* INSERT INTO game (s_name) VALUES('Rice University');
+ INSERT INTO School (s_name) VALUES('University of Colorado');
+ INSERT INTO School (s_name) VALUES('University of Houston');
+ INSERT INTO School (s_name) VALUES('UHCL');
+INSERT INTO School (s_name) VALUES('Trine University'); */
+
