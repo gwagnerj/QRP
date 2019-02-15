ae583c02535b1fd5100bca8aa34b57cad05fe96d
gwagnerj <38090331+gwagnerj@users.noreply.github.com>
2019-02-15 15:15:29 -0500

    Activate first pass is probably may be clase

--- QRactivatePblm.php
+++ QRactivatePblm.php
@@ -35,149 +35,100 @@ if ( ! isset($_GET['problem_id']) or ! isset($_GET['users_id']) ) {
 	// here we will check to see if all of the information is filled out if it is then 
 	// write the information to the assignment table and return to the QRrepo
 
+$instr_last = $iid = $assign_t_created = $assing_num = "";
+$prob_num = $pp_flag1 = $pp_flag2 = $pp_flag3 =$pp_flag4 = $reflect_flag = $explore_flag=  "";
+$university = $connect_flag = $society_flag = $postp_flag1 =$postp_flag2 = $postp_flag3 =  "";
 
 
-
-
-
-
+if(isset($_POST['Assig_num'])){
 	
-	// Define variables and initialize with empty values
-$username = $password = $confirm_password = $university = "";
-$username_err = $password_err = $confirm_password_err = $university_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate username
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT users_id FROM Users WHERE username = :username";
-        
-        if($stmt = $pdo->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(':username', $param_username, PDO::PARAM_STR);
-            
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-            
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                if($stmt->rowCount() == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-         
-        // Close statement
-        unset($stmt);
-    }
-    
-    // Validate password
-    if(empty(trim($_POST['password']))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST['password'])) < 6){
-        $password_err = "Password must have at least 6 characters.";
-    } else{
-        $password = trim($_POST['password']);
-    }
-    
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = 'Please confirm password.';     
-    } else{
-        $confirm_password = trim($_POST['confirm_password']);
-        if($password != $confirm_password){
-            $confirm_password_err = 'Password did not match.';
-        }
-    }
-	
-	// Validate university
-    if(empty(trim($_POST['university']))){
-        $password_err = "Please enter a school or organization.";     
-    } elseif(strlen(trim($_POST['university'])) < 2){
-        $university_err = "School or organization name must have at least 2 characters.";
-    } else{
-        $university = htmlentities(trim($_POST['university']));
-    }
-	// Validate email
-    if(empty(trim($_POST['email']))){
-        $email_err = "Please enter a valid email address that you check regularly.";     
-    } elseif(strlen(trim($_POST['email'])) < 4){
-        $email_err = "email input too short to be a valid email address.";
-    } else{
-        $email = htmlentities(trim($_POST['email']));
-    }
-	// Validate first name
-    if(empty(trim($_POST['first']))){
-        $first_err = "Please enter a valid first name.";     
-    } elseif(strlen(trim($_POST['first'])) < 2){
-        $first_err = "First name too short.";
-    } else{
-        $first = htmlentities(trim($_POST['first']));
-    }
-	// Validate last name
-    if(empty(trim($_POST['last']))){
-        $last_err = "Please enter a valid last name.";     
-    } elseif(strlen(trim($_POST['last'])) < 2){
-        $last_err = "last name too short.";
-    } else{
-        $last = htmlentities(trim($_POST['last']));
-    }
-	
-    
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($university_err) && empty($email_err) && empty($first_err) && empty($last_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO Users (username, password, university, security, email, first, last) VALUES (:username, :password, :university, :security,:email, :first, :last)";
+
+
+
+  // Prepare an insert statement
+        $sql = "INSERT INTO Assign (instr_last, iid, university, assign_t_created, assign_num, prob_num, pp_flag1, pp_flag2,pp_flag3, pp_flag4,reflect_flag,explore_flag,connect_flag,society_flag,postp_flag1,postp_flag2,postp_flag3)
+		VALUES (:instr_last, :iid,:university, :assign_t_created, :assign_num,:prob_num, :pp_flag1, :pp_flag2,:pp_flag3, :pp_flag4,:reflect_flag, :explore_flag,:connect_flag, :society_flag,:postp_flag1, :postp_flag2,:postp_flag3)";
          
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(':username', $param_username, PDO::PARAM_STR);
-            $stmt->bindParam(':password', $param_password, PDO::PARAM_STR);
-            $stmt->bindParam(':university', $param_university, PDO::PARAM_STR);
-            $stmt->bindParam(':security', $param_security, PDO::PARAM_STR);
-		    $stmt->bindParam(':email', $param_email, PDO::PARAM_STR);
-		    $stmt->bindParam(':first', $param_first, PDO::PARAM_STR);
-			$stmt->bindParam(':last', $param_last, PDO::PARAM_STR);
-			
+            $stmt->bindParam(':instr_last', $instr_last, PDO::PARAM_STR);
+            $stmt->bindParam(':iid', $iid, PDO::PARAM_STR);
+            $stmt->bindParam(':university', $university, PDO::PARAM_STR);
+            $stmt->bindParam(':assign_t_created', $assign_t_created, PDO::PARAM_STR);
+		    $stmt->bindParam(':assign_num', $assign_num, PDO::PARAM_STR);
+		    $stmt->bindParam(':prob_num', $prob_num, PDO::PARAM_STR);
+			$stmt->bindParam(':pp_flag1', $pp_flag1, PDO::PARAM_STR);
+			$stmt->bindParam(':pp_flag2', $pp_flag2, PDO::PARAM_STR);
+			$stmt->bindParam(':pp_flag3', $pp_flag3, PDO::PARAM_STR);
+			$stmt->bindParam(':pp_flag4', $pp_flag4, PDO::PARAM_STR);
+			$stmt->bindParam(':postp_flag1', $postp_flag1, PDO::PARAM_STR);
+			$stmt->bindParam(':postp_flag2', $postp_flag2, PDO::PARAM_STR);
+			$stmt->bindParam(':postp_flag3', $postp_flag3, PDO::PARAM_STR);
+			$stmt->bindParam(':connect_flag', $connect_flag, PDO::PARAM_STR);
+			$stmt->bindParam(':explore_flag', $explore_flag, PDO::PARAM_STR);
+			$stmt->bindParam(':reflect_flag', $reflect_flag, PDO::PARAM_STR);
+			$stmt->bindParam(':society_flag', $society_flag, PDO::PARAM_STR);
 			
             // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            $param_university = $university;
-			 $param_security = 'contrib';  //its either contrib or admin
-			 $param_email = $email;
-		     $param_first = $first; 
-		     $param_last = $last;
+           
+		   $assign_num = htmlentities($_POST['Assig_num']);
+			$instr_last = $Users_data['last'];
+			$iid = $Users_data['users_id'];
+			$assign_t_created = time();
+			$university = $Users_data['university'];
+			$prob_num = $_GET['problem_id'];
+			if(isset($_POST['q_on_q'])){
+				$pp_flag1 = 1;
+			}
+			if(isset($_POST['guess'])){
+				$pp_flag2 = 1;
+			}
+			if(isset($_POST['Prelim_MC'])){
+				$pp_flag3 = 1;
+			}
+			if(isset($_POST['Mics'])){
+				$pp_flag4 = 1;
+			}
+			if(isset($_POST['reflect'])){
+				$reflect_flag = 1;
+			}
+			if(isset($_POST['explore'])){
+				$explore_flag = 1;
+			}
+			if(isset($_POST['connect'])){
+				$connect_flag = 1;
+			}
+			if(isset($_POST['society'])){
+				$society_flag = 1;
+			}
+			if(isset($_POST['postprob1'])){
+				$postp_flag1 = 1;
+			}
+			if(isset($_POST['postprob2'])){
+				$postp_flag2 = 1;
+			}
+			if(isset($_POST['postprob3'])){
+				$postp_flag3 = 1;
+			}
+		   
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Redirect to login page
-                header("location: login.php");
+                header("location: QRPRepo.php");
             } else{
                 echo "Something went wrong. Please try again later.";
             }
         }
-         
-        // Close statement
+  // Close statement
         unset($stmt);
     }
     
     // Close connection
     unset($pdo);
+
 }
 	
-	
-	
 
-	
 	
 ?>	
 <!DOCTYPE html>
@@ -191,16 +142,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <body>
 <header>
-<h2>Activate Problem - Please select the options that you want with this problem</h2>
+<h2>Activate Problem - Please select the options that you want to assign with this problem</h2>
 </header>	
 	 <div class="wrapper">
        
         <form  method="post">
+		
+			<input type= "text" Name="Assig_num" required>
            <?php 
 				if(! empty(trim($problem_data['preprob_3']))){
 					echo ('<input type="checkbox" name="Prelim_MC" value="Prelim_MC_q"> Preliminary Multiple Choice Question <br>');
 				}
-			<?php 
+			
 				if(! empty(trim($problem_data['preprob_4']))){
 					echo ('<input type="checkbox" name="Mics" value="Prelim_misc"> Additional Preliminary Activities <br>');
 				}
@@ -209,7 +162,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			
 			<input type="checkbox" name="guess" value="Prelim_guess"> Preliminary Estimates <br>
 			<input type="checkbox" name="q_on_q" value="Prelim_q_on_q"> Questions about the Question <br>
+			<p> Reflections:</p>
 			
+			<input type="checkbox" name="reflect" value="reflection_reflect"> Reflect  <br>
+			<input type="checkbox" name="explore" value="reflection_explore"> Explore  <br>
+			<input type="checkbox" name="connect" value="reflection_connect"> Connect  <br>
+			<input type="checkbox" name="society" value="reflection_society"> Society  <br>
+			<input type = "submit">
 			
 			
 			
