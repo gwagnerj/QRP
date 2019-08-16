186d5d84ed1daa74e1bbf218601c0341110dfc12
John Wagner <gwagnerj@gmail.com>
2019-08-16 06:22:42 -0400

    Cleaning up old files

--- Files from Current production system 7 April 2019/index5.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.QRProblems.org/QRP/login.php"); /* Redirect browser to login this should be put in the QRProblems.org/login */
-exit();
-?>
--- Files from Current production system 7 April 2019/pdo.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo=new PDO ('mysql:host=localhost;port=3306;dbname=wagnerj_QRP','wagnerj_wagnerj','Iron26Men&FeMarines');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
