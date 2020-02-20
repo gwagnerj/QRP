0756452e216510295c5558e5567b912ee53e06b8
John Wagner <gwagnerj@gmail.com>
2020-02-19 19:44:36 -0500

    Just get the game number and go on

---
+++ QRGameMasterStart.php
@@ -0,0 +1,63 @@
+<?php
+	require_once "pdo.php";
+	session_start();
+	
+	
+	$game_id= '';
+
+// this will be called form the main repo when the game master wants to run a game
+// this is just to get the game number and go on to QRGMaster.php with a post of the game number.
+// Validity will be checked in that file and sent back here if it is not valid
+
+
+	?>
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<link rel="icon" type="image/png" href="McKetta.png" />  
+<meta Charset = "utf-8">
+<title>QRGame Master</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+<header>
+<h1>Quick Response Homework Bypass</h1>
+</header>
+
+<?php
+	if(isset($_POST['pin']) || isset($_POST['problem_id']) || isset($_POST['iid'])){
+		if ( isset($_SESSION['error']) ) {
+			echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+			unset($_SESSION['error']);
+		}
+		if ( isset($_SESSION['success']) ) {
+			echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
+			unset($_SESSION['success']);
+		}
+	}
+ 
+?>
+
+<!--<h3>Print the problem statement with "Ctrl P"</h3>
+ <p><font color = 'blue' size='2'> Try "Ctrl +" and "Ctrl -" for resizing the display</font></p>  -->
+<form action = "QRGMaster.php" method="POST">
+		
+        this should list the Active game numbers and possibly problem title - for now just get it working
+        
+        <p><font color=#003399>Game Number: </font><input type="number" name="game_id" id="game_id" size=3 value=<?php echo($game_id);?> ></p>
+
+
+	<p><input type = "submit" value="Submit" id="submit_id" size="2" style = "width: 30%; background-color: #003399; color: white"/> &nbsp &nbsp </p>  
+	</form>
+	<a href="QRPRepo.php">Finished / Cancel - go back to Repository</a>
+	
+
+
+</body>
+</html>
+
+
+
