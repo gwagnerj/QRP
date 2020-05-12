d5bc25599d64c96c1068324e372c77759b5d3a07
John Wagner <gwagnerj@gmail.com>
2020-05-12 07:55:48 -0400

    cleaned up still no replication

--- makeMoodleXML.php
+++ makeMoodleXML.php
@@ -1,12 +1,28 @@
 <?php
+//header( "content-type: application/xml; charset=ISO-utf-8" );
+     // Set the content type to be XML, so that the browser will   recognise it as XML.
+
 require_once "pdo.php";
 session_start();
+
 
 // This is the make a Moodle XML file of the problem
 
 $problem_id= '';
 
 
+// this next function modified from stack overflow https://stackoverflow.com/questions/5696412/how-to-get-a-substring-between-two-strings-in-php
+
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    if (strpos($string, $end)!=false){
+        $len = strpos($string, $end, $ini) - $ini;
+    } else {$len = strlen($string)-$ini;}
+    return substr($string, $ini, $len);
+}
 
 if(isset($_POST['problem_id'])){
 	$problem_id = htmlentities($_POST['problem_id']);
@@ -32,7 +48,6 @@ if(isset($_POST['iid'])){
         echo "</script>";
 }
 
-	$dex = 2;
 
 
  $sql = "SELECT * FROM Problem WHERE problem_id = :problem_id";
@@ -40,108 +55,34 @@ if(isset($_POST['iid'])){
     $stmt->execute(array(':problem_id' => $problem_id));
 	$pblm_data = $stmt -> fetch();
 	
-//echo($data['title']);
 
-
-
-    $stmt = $pdo->prepare("SELECT * FROM Input where problem_id = :problem_id AND dex = :dex");
-	$stmt->execute(array(":problem_id" => $_POST['problem_id'], ":dex" => $dex));
-	$row = $stmt->fetch();
-
-    $stmt = $pdo->prepare("SELECT * FROM Qa where problem_id = :problem_id AND dex = :dex");
-	$stmt->execute(array(":problem_id" => $_POST['problem_id'], ":dex" => $dex));
-	$row_ans = $stmt->fetch();
 
 // read in the names of the variables for the problem
     $nv = 0;  // number of non-null variables
    for ($i = 0; $i <= 13; $i++) {
     
-        if($pblm_data['nv_'.($i+1)]!='Null'&& $row['v_'.($i+1)]!='Null' ){
+        if($pblm_data['nv_'.($i+1)]!='Null' ){
             $nvar[$i]=$pblm_data['nv_'.($i+1)];
             $nv++;
-          // echo($nvar[$i]." ");
-            $vari[$i] = $row['v_'.($i+1)];
-         // echo($vari[$i]." ");
-          
             $pattern[$i]= '/##'.$nvar[$i].'.+?##/';
          // echo ($pattern[$i]);
          }
-        
-      
    }
    //echo $nv;
-   
-  
-   
-  // Read in the tolerances, units  and answers for parts of problem
-  $last_part = 0;
    $i = 0;
-   for ($m = 'a'; $m<='j'; $m++){
-     $tol[$i] = $pblm_data['tol_'.($m)];
+    for ($m = 'a'; $m<='j'; $m++){
+        $tol[$i] = $pblm_data['tol_'.($m)];
+        $i++;
+    }
+  //-----------------------------------------------------creating header for HTML file -------------------------------
       
-      if($pblm_data['units_'.($m)]!='Null'){
-        $units[$i] = $pblm_data['units_'.($m)];
-      }
-     
-    
-        $ans[$i]= $row_ans['ans_'.$m];
-       if($row_ans['ans_'.$m]<1.2e43){
-           $last_part = $i+1;
-            $MOE[$i]= $tol[$i]/1000 * $ans[$i];
-            $ans_pattern[$i] = "{1:NUMERICAL:=".$ans[$i].":".$MOE[$i]."}";
-     // echo($ans_pattern[$i]);
-     }
-      
-    // echo ($units[$i]." ");
-   // echo ($tol[$i]." ");
-    $i++;
-   }
-  // echo('last part = '.$last_part);
-   // read in the answers for the variables
-  
-
-   
-   
-   
-   
-
-
-
-$xml_file_name = substr($pblm_data['htmlfilenm'], 0, strrpos($pblm_data['htmlfilenm'], "."));
-$xml_file_name = $xml_file_name.'.xml';
-
-$htmlfilenm = "uploads/".$pblm_data['htmlfilenm'];
-
-
-// this next function modified from stack overflow https://stackoverflow.com/questions/5696412/how-to-get-a-substring-between-two-strings-in-php
-function get_string_between($string, $start, $end){
-    $string = ' ' . $string;
-    $ini = strpos($string, $start);
-    if ($ini == 0) return '';
-    $ini += strlen($start);
-    if (strpos($string, $end)!=false){
-        $len = strpos($string, $end, $ini) - $ini;
-    } else {$len = strlen($string)-$ini;}
-    return substr($string, $ini, $len);
-}
-
-    
-
-
-        // Set the content type to be XML, so that the browser will   recognise it as XML.
-       //   header( "content-type: application/xml; charset=ISO-utf-8" );
 
         // "Create" the document.
-      //  $xml = new DOMDocument( "1.0", "ISO-8859-15" );
       
         $xml = new DOMDocument();
-
                 $xml->encoding = 'utf-8';
-
                 $xml->xmlVersion = '1.0';
-
                 $xml->formatOutput = true;
-      
 
         // Create some elements.
        
@@ -151,7 +92,7 @@ function get_string_between($string, $start, $end){
             // Set the attributes.
             $xml_question->setAttribute( "type", "category" );
             $xml_category = $xml->createElement( "category"); 
-             $xml_question->appendChild( $xml_category );
+            $xml_question->appendChild( $xml_category );
            
             $xml_text = $xml->createElement( "text","\$course\$/top/CategoryName"); 
              $xml_category->appendChild( $xml_text );
@@ -172,7 +113,11 @@ function get_string_between($string, $start, $end){
             $xml_category = $xml->createElement( "category"); 
              $xml_question->appendChild( $xml_category );
            
-            $xml_text = $xml->createElement( "text","\$course\$/top/CategoryName/Testing XML up1"); 
+                      // the catagory name should be the problem title
+           $cat_name = '\$course\$/top/CategoryName/'.$pblm_data['title'];
+           
+          //  $xml_text = $xml->createElement( "text","\$course\$/top/CategoryName/Testing XML up1"); 
+             $xml_text = $xml->createElement( "text", $cat_name); 
              $xml_category->appendChild( $xml_text );
             
             
@@ -181,24 +126,66 @@ function get_string_between($string, $start, $end){
             // Append category and info to question.
            
             $xml_question->appendChild( $xml_info );
-       
+
         $xml_quiz->appendChild( $xml_question );
  
-             $xml_question1 = $xml->createElement( "question");
+   
+   
+  for($dex=2; $dex<3; $dex++){
+      
+    $stmt = $pdo->prepare("SELECT * FROM Input where problem_id = :problem_id AND dex = :dex");
+	$stmt->execute(array(":problem_id" => $_POST['problem_id'], ":dex" => $dex));
+	$row = $stmt->fetch();
+
+    $stmt = $pdo->prepare("SELECT * FROM Qa where problem_id = :problem_id AND dex = :dex");
+	$stmt->execute(array(":problem_id" => $_POST['problem_id'], ":dex" => $dex));
+	$row_ans = $stmt->fetch();
+
+ 
+    $xml_file_name = substr($pblm_data['htmlfilenm'], 0, strrpos($pblm_data['htmlfilenm'], "."));
+    $xml_file_name = $xml_file_name.'.xml';
+
+    $htmlfilenm = "uploads/".$pblm_data['htmlfilenm'];
+
+
+
+//------------------------------------Get index specific data---------------------------- 
+   // Read in the value for the input variables
+   
+    for ($i = 0; $i <= $nv; $i++) {
+    
+        if($row['v_'.($i+1)]!='Null' ){
+            $vari[$i] = $row['v_'.($i+1)];
+         // echo ($pattern[$i]);
+        }
+    }
+   
+  // set the answer and margin of error into a pattern
+   $last_part = 0;
+   $i = 0;
+   for ($m = 'a'; $m<='j'; $m++){
+        $ans[$i]= $row_ans['ans_'.$m];
+       if($row_ans['ans_'.$m]<1.2e43){
+           $last_part = $i+1;
+            $MOE[$i]= $tol[$i]/1000 * $ans[$i];
+            $ans_pattern[$i] = "{1:NUMERICAL:=".$ans[$i].":".$MOE[$i]."}";
+        }
+     $i++;
+    }
+
+
+  //--------------------------------make the xml specific for the problem index------------------------------------------------------------------------------------------      
+           
+           $xml_question1 = $xml->createElement( "question");
 
             // Set the attributes.
-         
             $xml_question1->setAttribute( "type", "cloze" );
-            
-            $cloze = 'cloze1';
-            
+            $version = 'version_'.$dex;
             $xml_name = $xml->createElement( "name");
-             $xml_text = $xml->createElement( "text",  $pblm_data['title'] );
+             $xml_text = $xml->createElement( "text", $version );
              $xml_name->appendChild( $xml_text );
-            
          
             $xml_question1->appendChild( $xml_name );
-              
 
             $xml_comment = $xml->createComment('this is my comment'); 
              $xml_question1->appendChild( $xml_comment );
@@ -226,148 +213,58 @@ function get_string_between($string, $start, $end){
                     if($ans[$i]<1.2e43){
                        // if($i != $last_part-1){
                           $string1= $m."==p";
-                          
                           $string2 = 'p=='.$q;
-                        
                           $html_q[$i] = trim(get_string_between($htmlQuestions,$string1,$string2));
-                           
+                          
                             // just in case there are some variables in the questions we need to substitute for them
                            for( $j=0;$j<$nv;$j++){
                                     $html_q[$i] = preg_replace($pattern[$j],$vari[$j],$html_q[$i]);
                                 }
                             // now add the questions and answer pattern to the stem for the complete 
-                            
                             $html_question = $html_question.$m.")&nbsp;".$html_q[$i].'<br>&nbsp;&nbsp;&nbsp;&nbsp;'.$ans_pattern[$i]."<br><br>";
                         $m++;
                         $q++;
                       //  } 
                     }
                 }
-              // get rid of alll the paragraph formatting in the question block with a regular expression
-           // $html_question = preg_replace('/<p\b[^>]*>(.*?)<\/p>/i', '', htmlentities($html_question));
+     
            
-           
-          
-            
-            
-           // $html_question = preg_replace('/<p\b[^>]*>(.*?)<\/p>/i', '', $result['title']);
-            // echo ('we should have stripped the paragraphs out '.$html_question);
              // now add the stem
               $html_question = $html_stem.'<br>'.$html_question;
-              $html_question = strip_tags($html_question,'<br>,<img>,<sub>,<sup>,</sub></sup>');
+              $html_question = strip_tags($html_question,'<br>,<img>,<sub>,<sup>,</sub></sup>');  // strip out the tags except the ones in the secound argument
          
          // need to take care of the images     
          
               $dom = new DOMDocument();
                $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html_question);
 
-              // $dom->loadHTML($html_question,LIBXML_HTML_NODEFDTD);
                 $images = $dom->getElementsByTagName('img');
-              //   C:\xampp\htdocs\QRP\uploads\p529_0_hydroelectric Pump Storage_files
-              
             
                 foreach ($images as $image) {
-                   
-                 $src = $image->getAttribute('src');
-                
-                 $src = 'uploads/'.$src;
-                 
-                 // $src = urldecode(str_replace('/','\\', $src));  // comment this out when not on the local system
-         
-                 $src = urldecode($src);
-                 // echo('source of image is '.$src);
-                     $type = pathinfo($src, PATHINFO_EXTENSION);
-                
-                    //  $new_src = base64_encode(file_get_contents($src));
-                      $base64 = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents($src));
-                    //  echo ($base64);
-                            
-                      $image->setAttribute("src", $base64); 
-                       
-                      $html_question = $dom->saveHTML();
-                    //echo substr($dom->saveHTML(), 12, -15); // the star of this operation
-                     // $html_question = $dom->load(WTF);
-                     //  $html_question = $dom->loadHTML('wtf');
- 
-                 //  echo '<pre>' . htmlspecialchars($dom->saveHTML()) . '</pre>';
-                    
-
-
-
-
-                //    $dom ->SaveHtml();
-                //  $html = '<html>....</html>';
-                 //   $dom = new DOMDocument();
-                //    $dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-                //    $images = $dom->getElementsByTagName('img');
-                //    foreach ($images as $image) {
-                //            $src = $image->getAttribute('src');
-                //            $type = pathinfo($src, PATHINFO_EXTENSION);
-                //            $data = file_get_contents($src);
-                 
-                         
-                    }
-                   // $html = $dom->saveHTML();
-                  
-                  
-                  
-                  
-                 
-                  
-                  
-                  
-                  
-                  
-                   // echo('encoded image : '.$new_src);
-                    
+                     $src = $image->getAttribute('src');
+                     $src = 'uploads/'.$src;
                      
-                 /*  
-                 */
-                
-                // $new_src = 'image/products/newimage.jpg';
-               //  $image->setAttribute('src', $new_src);
-              //   $image->setAttribute('data-src', $old_src);
-                
-                 
-              //  $data['description'] = $dom->saveHTML();
+                     // $src = urldecode(str_replace('/','\\', $src));  // comment this out when not on the local system
              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-         //   $html_q_a = get_string_between(file_get_contents($htmlfilenm),'a==p','p==b');  // what if part a is not numerical we need to check using the answers  also these should be an array
-          //    $html_q_b = get_string_between(file_get_contents($htmlfilenm),'a==p','p==b'); // what if part b does not exist of is the last one
-        //     echo($html_q_a);
-             
-           
-             
-             
+                     $src = urldecode($src);
+                     $type = pathinfo($src, PATHINFO_EXTENSION);
+                     $base64 = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents($src));
+                     $image->setAttribute("src", $base64); 
+                     $html_question = $dom->saveHTML();
+                         
+               }
+   
              
              $xml_text1 = $xml->createElement( "text");
              
-               $xml_questiontext->appendChild( $xml_text1 );
+             $xml_questiontext->appendChild( $xml_text1 );
           
-                     $xml_text1->appendChild(
+              $xml_text1->appendChild(
                       $xml->createCDATASection($html_question)
                     );
-                     
-           
-
                     
            $xml_question->appendChild( $xml_questiontext );
             $xml_question1->appendChild( $xml_questiontext );
-       
-            
            
             $xml_feedback = $xml->createElement( "generalfeedback" );
             $xml_feedback->setAttribute( "format", "html" );
@@ -384,20 +281,8 @@ function get_string_between($string, $start, $end){
             $xml_question1->appendChild( $xml_idnumber );
             
          $xml_quiz->appendChild( $xml_question1 );
-
-         
-
-
-
-
-
-
-
-     
-        
         
          // $xml_quiz->appendChild( $xmlCDATA );
-          
           
           $xml->appendChild( $xml_quiz );
         
@@ -405,12 +290,9 @@ function get_string_between($string, $start, $end){
         print $xml->saveXML();
          
         // $xmlData  = $xml->saveXML();
-
             $xml->save($xml_file_name);
 
-
-
-
+      }
 
 
 
