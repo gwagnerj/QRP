82a6be4c7c060c21460f9482cb4cb3873fd109de
John Wagner <gwagnerj@gmail.com>
2020-05-13 09:36:58 -0400

    Jamies mod to relative file

--- downloadDocx.php
+++ downloadDocx.php
@@ -38,7 +38,7 @@ session_start();
     echo 'The latest QR Solver excel macro enabled template is below - you may have to enable macros to use it.';
 	echo "<br>";
 	echo "<br>";
-    echo '<a href="downloads/QRP solver A500L.xltm"> QRP solver A500L.xltm </a>';
+    echo '<a href="downloads/QRP solver A500M.xltm"> QRP solver A500M.xltm </a>';
 	echo "<br>";
 	echo "<hr>";
 
