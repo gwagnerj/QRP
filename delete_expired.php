2147b0eaa5f9507d0282fe69ff29e2ec77a15fcf
John Wagner <gwagnerj@gmail.com>
2020-05-20 06:26:24 -0400

    delete expired exam activity?

--- delete_expired.php
+++ delete_expired.php
@@ -5,25 +5,6 @@ session_unset();
 session_start();
 $_SESSION['checker']=1;
 
-?>
-
-<!DOCTYPE html>
-<html lang = "en">
-<head>
-<link rel="icon" type="image/png" href="McKetta.png" />  
-<meta Charset = "utf-8">
-<title>checker Bypass</title>
-<meta name="viewport" content="width=device-width, initial-scale=1" /> 
-</head>
-
-<body>
-<header>
-<h1>Delete Expired Entries </h1>
-</header>
-
-
-
-<?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 	
 	if (isset($_POST['deleteClass'])){
@@ -46,7 +27,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 			$stmt = $pdo->prepare($sql);
 			$stmt -> execute();
 	}
-	
+	if (isset($_POST['deleteExam'])){
+			$sql = "SELECT * FROM `Exam` WHERE `exp_date` < CURDATE()";
+			$stmt = $pdo->prepare($sql);
+			$stmt -> execute();
+            $row =$stmt ->fetch();
+            $iid = $row['iid'];
+            $currentclass_id = $row['currentclass_id'];
+           
+           $sql = "DELETE FROM `Examactivity` WHERE `iid` = :iid AND `currentclass_id` = :currentclass_id ";
+			$stmt = $pdo->prepare($sql);
+			$stmt -> execute(array(
+                ':iid'=>$iid,
+                ':currentclass_id'=>$currentclass_id ));
+          
+            $sql = "DELETE FROM `Exam` WHERE `exp_date` < CURDATE()";
+			$stmt = $pdo->prepare($sql);
+			$stmt -> execute();
+
+          
+    
+    }
+    
+    
+	if (isset($_POST['deleteUsers'])){
+			$sql = "DELETE FROM `Users` WHERE `exp_date` < CURDATE()";
+			$stmt = $pdo->prepare($sql);
+			$stmt -> execute();
+	}
+    
+    
+    
+    
 	
 	header( 'Location: QRPRepo.php' ) ;
 	 
@@ -66,12 +78,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 
+
+
+
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<link rel="icon" type="image/png" href="McKetta.png" />  
+<meta Charset = "utf-8">
+<title>checker Bypass</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+</head>
+
+<body>
+<header>
+<h1>Delete Expired Entries </h1>
+</header>
+
+
+
+
 
 <form  method = "POST" >
 	<font color=#003399>Expired CurrentClasses and Activity of Expired CurrentClasses</font><input type="checkbox" name="deleteClass" checked > &nbsp;&nbsp;&nbsp;
 	<font color=#003399>Expired Users </font><input type="checkbox" name="deleteUsers" checked > &nbsp;&nbsp;&nbsp;
 	<font color=#003399>Expired Assignments </font><input type="checkbox" name="deleteAssignment" checked > &nbsp;&nbsp;&nbsp;
+   	<font color=#003399>Expired Exams and Activity of Expired Exam </font><input type="checkbox" name="deleteExam" checked > &nbsp;&nbsp;&nbsp;
+
 	</br>
 	<!-- <p><font color=#003399>Grading Scheme Number: </font><input type="text" name="gs_num" size=3 value="<?php echo (htmlentities($gs_num))?>"  ></p> -->
 
